97e8a2f1d3399324e16aab0fd66afc51395a3b79
Orcas <anake2333@gmail.com>
2023-06-07 14:40:55 +0800

    增加商品可售性检查接口

--- src/main/java/com/orcas/model/response/product/ProductSaleState.java
+++ src/main/java/com/orcas/model/response/product/ProductSaleState.java
@@ -10,8 +10,25 @@ import java.io.Serializable;
 @Data
 public class ProductSaleState implements Serializable {
   public static final long serialVersionUID = 1L;
-  /** skuId */
-  private Long sku;
-  /** 1：上架，0：下架 */
-  private Integer state;
+  /** 商品编号 */
+  private Long skuId;
+  /** 商品名称 */
+  private String name;
+  /** 是否可售，1：是，0：否 */
+  private Integer saleState;
+  /** 是否可开专票，1：支持，0：不支持 */
+  private Integer isCanVAT;
+  /**
+   * 无理由退货类型：0,1,2,3,4,5,6,7,8. 0、3：不支持7天无理由退货； 1、5、8或null：支持7天无理由退货； 2：支持90天无理由退货； 4、7：支持15天无理由退货；
+   * 6：支持30天无理由退货； （提示客户取到其他枚举值，无效）
+   */
+  private Integer noReasonToReturn;
+  /**
+   * 无理由退货文案类型： null：文案空； 0：文案空； 1：支持7天无理由退货； 2：支持7天无理由退货（拆封后不支持）； 3：支持7天无理由退货（激活后不支持）
+   * 4：支持7天无理由退货（使用后不支持）； 5：支持7天无理由退货（安装后不支持）； 12：支持15天无理由退货； 13：支持15天无理由退货（拆封后不支持）；
+   * 14：支持15天无理由退货（激活后不支持）； 15：支持15天无理由退货（使用后不支持）； 16：支持15天无理由退货（安装后不支持）； 22：支持30天无理由退货；
+   * 23：支持30天无理由退货（安装后不支持）； 24：支持30天无理由退货（拆封后不支持）； 25：支持30天无理由退货（使用后不支持）； 26：支持30天无理由退货（激活后不支持）；
+   * （提示客户取到其他枚举值，无效）
+   */
+  private Integer thwa;
 }
