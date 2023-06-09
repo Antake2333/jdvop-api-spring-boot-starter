2322e5457938fabfcfce8afce25c5c22a8df0a95
Orcas <anake2333@gmail.com>
2023-06-09 14:05:33 +0800

    增加新订单列表查询

--- src/main/java/com/orcas/enums/ApiEnum.java
+++ src/main/java/com/orcas/enums/ApiEnum.java
@@ -2,6 +2,8 @@ package com.orcas.enums;
 
 import com.orcas.model.request.order.OrderConfirmReceivedBatchRequest;
 import com.orcas.model.request.order.OrderConfirmReceivedRequest;
+import com.orcas.model.request.order.OrderQueryNewListRequest;
+import com.orcas.model.request.order.OrderSaveOrUpdatePoNoRequest;
 
 /**
  * @Description @Author LinLei @Date 2023/6/9
@@ -10,7 +12,8 @@ public enum ApiEnum {
   /** 订单确认收货 */
   ORDER_CONFIRM_RECEIVED("api/order/confirmReceived", OrderConfirmReceivedRequest.class, "订单确认收货"),
   ORDER_CONFIRM_RECEIVED_BATCH("api/order/batchConfirmReceived", OrderConfirmReceivedBatchRequest.class, "订单批量确认收货"),
-  ORDER_SAVE_OR_UPDATE_PONO("api/order/saveOrUpdatePoNo", OrderConfirmReceivedBatchRequest.class, "更新采购单号"),
+  ORDER_SAVE_OR_UPDATE_PONO("api/order/saveOrUpdatePoNo", OrderSaveOrUpdatePoNoRequest.class, "更新采购单号"),
+  ORDER_QUERY_NEW_LIST("api/checkOrder/checkNewOrder", OrderQueryNewListRequest.class, "查询新建订单列表"),
   ;
   private final String url;
   private final Class<?> requestClass;
---
+++ src/main/java/com/orcas/model/response/order/OrderQueryNewListResponse.java
@@ -0,0 +1,42 @@
+package com.orcas.model.response.order;
+
+import com.orcas.model.response.BaseResponse;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @Description @Author LinLei @Date 2023/6/9
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class OrderQueryNewListResponse extends BaseResponse {
+  /** 订单总数 */
+  private Integer total;
+  /** 总页码数 */
+  private Integer totalPage;
+  /** 当前页码 */
+  private Integer curPage;
+  /** 订单信息列表 */
+  private List<Order> orders;
+
+  @Data
+  public static class Order implements Serializable {
+    public static final long serialVersionUID = 1L;
+    /** 京东订单编号 */
+    private Long jdOrderId;
+    /** 订单状态 0 是新建 1是妥投 2是拒收 */
+    private Integer state;
+    /** 是否挂起 0为为挂起 1为挂起 */
+    private Integer hangUpState;
+    /** 开票方式(1为随货开票，0为订单预借，2为集中开票 ) */
+    private Integer invoiceState;
+    /** 订单金额 */
+    private BigDecimal orderPrice;
+    /** 订单创建时间，格式： yyyy-MM-dd HH:mm:ss */
+    private String time;
+  }
+}
