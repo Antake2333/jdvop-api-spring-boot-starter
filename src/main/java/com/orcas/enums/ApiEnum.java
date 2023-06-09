d04f35ff551134067b6306de5d26c1724a0985cd
Orcas <anake2333@gmail.com>
2023-06-09 14:08:32 +0800

    增加妥投订单列表查询

--- src/main/java/com/orcas/enums/ApiEnum.java
+++ src/main/java/com/orcas/enums/ApiEnum.java
@@ -1,9 +1,6 @@
 package com.orcas.enums;
 
-import com.orcas.model.request.order.OrderConfirmReceivedBatchRequest;
-import com.orcas.model.request.order.OrderConfirmReceivedRequest;
-import com.orcas.model.request.order.OrderQueryNewListRequest;
-import com.orcas.model.request.order.OrderSaveOrUpdatePoNoRequest;
+import com.orcas.model.request.order.*;
 
 /**
  * @Description @Author LinLei @Date 2023/6/9
@@ -14,6 +11,7 @@ public enum ApiEnum {
   ORDER_CONFIRM_RECEIVED_BATCH("api/order/batchConfirmReceived", OrderConfirmReceivedBatchRequest.class, "订单批量确认收货"),
   ORDER_SAVE_OR_UPDATE_PONO("api/order/saveOrUpdatePoNo", OrderSaveOrUpdatePoNoRequest.class, "更新采购单号"),
   ORDER_QUERY_NEW_LIST("api/checkOrder/checkNewOrder", OrderQueryNewListRequest.class, "查询新建订单列表"),
+  ORDER_QUERY_DLOK_LIST("api/checkOrder/checkDlokOrder", OrderQueryDlokListRequest.class, "查询妥投订单列表"),
   ;
   private final String url;
   private final Class<?> requestClass;
